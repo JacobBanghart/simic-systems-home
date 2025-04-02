2a37d828638c16e969c6a9f730a20cca1f72e5e4
JacobBanghart <jacobmbanghart@gmail.com>
2025-04-02 18:04:53 -0500

    fix: set react-dom to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://simic.systems",
   integrations: [mdx(), sitemap(), react({
     experimentalReactChildren: true
   })],
@@ -17,4 +17,14 @@ export default defineConfig({
       enabled: true,
     },
   }),
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      // @ts-ignore
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
