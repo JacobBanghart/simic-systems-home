3fa06d219fa432f0dc7dfbf219f8c56f09855f07
JacobBanghart <jacobmbanghart@gmail.com>
2025-04-02 19:34:06 -0500

    feat: add backing paper

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -11,7 +11,7 @@ function CustomTabPanel(props: TabPanelProps) {
     const { children, value, index, ...other } = props;
 
     return (
-        <div
+        <Paper
             role="tabpanel"
             hidden={value !== index}
             id={`simple-tabpanel-${index}`}
@@ -19,7 +19,7 @@ function CustomTabPanel(props: TabPanelProps) {
             {...other}
         >
             {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-        </div>
+        </Paper>
     );
 }
 function a11yProps(index: number) {
