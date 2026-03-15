cf26a6a3e6fab59708f4b5c96bfffcff8767cc6e
JacobBanghart <jacobmbanghart@gmail.com>
2026-03-15 03:15:09 -0700

    fix: use trailingSlash ignore to stop 308 redirecting Stripe webhook POSTs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
   site: "https://simic.systems",
-  trailingSlash: "always",
+  trailingSlash: "ignore",
   integrations: [sitemap({
     filter: (page) =>
       !page.includes('/checkout/') &&
